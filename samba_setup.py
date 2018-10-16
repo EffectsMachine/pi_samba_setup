0ec5620f56105cca0ca9138a43f74c19d777ab67
EffectsMachine <42383550+EffectsMachine@users.noreply.github.com>
2018-10-16 10:08:11 +0800

    Add files via upload

---
+++ samba_setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+# File name   : motor.py
+# Description : Control Motors 
+# Website     : www.adeept.com
+# E-mail      : support@adeept.com
+# Author      : William
+# Date        : 2018/10/12
+
+import os
+import time
+
+def replace_num(file,initial,new_num):  
+    newline=""
+    str_num=str(new_num)
+    with open(file,"r") as f:
+        for line in f.readlines():
+            if(line.find(initial) == 0):
+                line = (str_num+'\n')
+            newline += line
+    with open(file,"w") as f:
+        f.writelines(newline)
+
+for x in range(1,4):
+	if os.system("sudo apt-get update") == 0:
+		break
+
+for x in range(1,4):
+	if os.system("sudo apt-get install samba samba-common-bin") == 0:
+		break
+
+try:
+	replace_num("//etc/samba/smb.conf",';   write list = root, @lpadmin',';   write list = root, @lpadmin\n\n[NASshare]\n   comment = raspberry\n   valid users = pi,root\n   path = /home/\n   browseable = yes\n   create mask = 0777\n   directory mask = 0777')
+except:
+	pass
+
+for x in range(1,4):
+	if os.system("sudo smbpasswd -a pi && 123 && 123") == 0:
+		break
+
+for x in range(1,4):
+	if os.system("sudo /etc/init.d/samba restart") == 0:
+		break
+
+print('name NASshare\npasswd 123')
